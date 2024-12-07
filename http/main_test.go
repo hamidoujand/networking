a4a62844354af8f8213fbbcc22f44c94421dcbeb
Hamid Oujand <hmdrzadev@gmail.com>
2024-12-07 15:25:36 +0330

    add simple mux with mid support

--- http/main_test.go
+++ http/main_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"io"
+	"io/ioutil"
 	"net"
 	"net/http"
 	"net/http/httptest"
@@ -54,4 +57,83 @@ func TestRestrictPrefix(t *testing.T) {
 			t.Errorf("%d: expected %d; actual %d", i, c.code, actual)
 		}
 	}
+}
+
+func TestSimpleMuxWithMiddleware(t *testing.T) {
+	serveMux := http.NewServeMux()
+
+	serveMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusNoContent)
+	})
+
+	serveMux.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprint(w, "Hello friend.")
+	})
+
+	serveMux.HandleFunc("/hello/there/", func(w http.ResponseWriter, r *http.Request) {
+		_, _ = fmt.Fprint(w, "Why, hello there.")
+	})
+
+	//apply middlewares
+	mux := applyMiddlewares(serveMux, drainAndClose)
+
+	testCases := []struct {
+		path     string
+		response string
+		code     int
+	}{
+		{"http://test/", "", http.StatusNoContent},
+		{"http://test/hello", "Hello friend.", http.StatusOK},
+		{"http://test/hello/there/", "Why, hello there.", http.StatusOK},
+		{"http://test/hello/there", "<a href=\"/hello/there/\">Moved Permanently</a>.\n\n",
+			http.StatusMovedPermanently},
+		{"http://test/hello/there/you", "Why, hello there.", http.StatusOK},
+		{"http://test/hello/and/goodbye", "", http.StatusNoContent},
+		{"http://test/something/else/entirely", "", http.StatusNoContent},
+		{"http://test/hello/you", "", http.StatusNoContent},
+	}
+
+	for i, c := range testCases {
+		r := httptest.NewRequest(http.MethodGet, c.path, nil)
+		w := httptest.NewRecorder()
+
+		mux.ServeHTTP(w, r)
+		resp := w.Result()
+
+		if actual := resp.StatusCode; c.code != actual {
+			t.Errorf("%d: expected code %d; actual %d", i, c.code, actual)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
+		_ = resp.Body.Close()
+		if actual := string(b); c.response != actual {
+			t.Errorf("%d: expected response %q; actual %q", i,
+				c.response, actual)
+		}
+	}
+}
+
+func drainAndClose(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		//call next handler
+		next.ServeHTTP(w, r)
+
+		//drain body
+		_, _ = io.Copy(io.Discard, r.Body)
+		_ = r.Body.Close()
+	})
+}
+
+type Middleware func(http.Handler) http.Handler
+
+func applyMiddlewares(mux http.Handler, mids ...Middleware) http.Handler {
+	for i := len(mids) - 1; i >= 0; i-- {
+		m := mids[i]
+		if m != nil {
+			mux = m(mux)
+		}
+	}
+	return mux
 }
